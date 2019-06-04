438e3ec872b6299fac1657e35db74eeca02bc36d
jules <juleswilfrid9@gmail.com>
2019-06-03 22:53:40 -0500

    tyu

--- Decoratemytexte/decoratemytext.js
+++ Decoratemytexte/decoratemytext.js
@@ -1,32 +1,13 @@
  
-var size;
+ var size;
 window.onload=pageLoad;
 
 function pageLoad() {
-    document.getElementById("bigger").onclick = enlargeText;
-    document.getElementById("igapy").onclick = pigLatinize;
-    document.getElementById("malkovise").onclick = malkovise;
-    document.getElementById("bling").onclick = blingText;
+         
+    document.getElementById("te").onclick = blingText;
 }
-function decoration(){
-alert("Hello, world!");
-}
- 
-function increaseFontSize() {
-    var el = document.getElementById('text');
-    var style = window.getComputedStyle(el, null).getPropertyValue('font-size');
-    var size = parseInt(style);
-        size += 2;
-    document.getElementById("text").style.fontSize = size + "pt";
-}
-function enlargeText() {
-    "use strict";
-
-    setInterval(increaseFontSize, 500);
-}
-
-function blingText() {
-    if (document.getElementById("bling").checked) {
+ function blingText() {
+    if (document.getElementById("te").checked) {
         document.body.style.backgroundImage = 'url("http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg")';
         document.getElementById("text").className = "blingtext";
     } else {
@@ -34,45 +15,52 @@ function blingText() {
         document.getElementById("text").className = " ";
     }
 }
-function pigLatinize() {
-    var ipText = document.getElementById("text").value.trim();
-    var ipWords = ipText.split(/\s+/);
-    var result = "";
-    for (var i = 0; i < ipWords.length; i++) {
-        result += pigLatinizeWord(ipWords[i]) + " ";
-    }
-    document.getElementById("text").value = result;
-
+ function mydeco() {
+  alert("Hello,World !");
+  var checkboxf=document.getElementById('textt');
+  var checkboxf=document.getElementById('textarea');
+ 
+ if(checkboxf.checked==true){
+  document.getElementById('textarea').style.fontWeight="bold";
+  document.getElementById('textarea').style.color="blue";
+  
+   
+  }
+  else{
+   document.getElementById('textarea').style.fontWeight="normal";
+    
+}
+}
+ 
+ 
+ 
+ 
+function myFunction() {
+  var elem = document.getElementById("textarea");
+  var theCSSprop = window.getComputedStyle(elem, null).getPropertyValue("font-size");
+  var mysize=parseFloat(theCSSprop);
+  alert(mysize);
+  elem.style.fontSize=(mysize+2)+"px";
+  alert(elem.style.fontSize);
+  
 }
 
-function malkovise() {
-    var ipText = document.getElementById("text").value;
-    var ipWords = ipText.split(/\s+/);
-    var result = "";
-    for (var i = 0; i < ipWords.length; i++) {
-        result += (isMalkovich(ipWords[i]) ? "Malkovitch" : ipWords[i]) + " ";
-    }
-    document.getElementById("text").value = result;
+var run0=0;
+var interval;
 
+function changeInterval(){
+	
+	if(run0==0){
+		interval=setInterval(myFunction,500);
+		run0 += 1;
+	}
+	else if(run0==1){
+		clearInterval((interval));
+		run0=0;
+	}
 }
-
-function isVowel(s) {
-    return (/^[aeiou]$/i).test(s);
-}
+ 
 
 
-function pigLatinizeWord(input) {
-    if (isVowel(input.charAt(0))) {
-        input += "ay";
-    } else {
-        input = input.substr(1) + input.charAt(0) + "ay";
-    }
-    return input;
-}
 
-function isMalkovich(s) {
-    if (s.length >= 5) {
-        return true;
-    }
-    return false;
-}
+
